0817e0028d8d804a867ec37e26fb5fa17f918580
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-06-20 14:02:58 -0500

    init commit

---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  title: 'Chase Lab',
+  tagline: '',
+  url: '',
+  baseUrl: '/',
+  favicon: 'img/uno.png',
+  organizationName: 'University of Nebraska at Omaha', // Usually your GitHub org/user name.
+  projectName: 'Chase Lab', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Chase Lab',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/cat.png',
+      },
+      links: [
+        // {
+        //   to: 'docs/',
+        //   activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 's',
+      links: [
+        {
+          title: 'Related Links',
+          items: [
+            {
+              label: 'ToxoProject Website',
+              to: 'https://toxoproject.com/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Chase Lab, Inc.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          routeBasePath: '/',
+          homePageId: 'dlc-slurm',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        themes: ['@docusaurus/theme-classic', '@docusaurus/theme-live-codeblock', '@docusaurus/theme-search-algolia'],
+      },
+    ],
+  ],
+};
