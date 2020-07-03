01cf813a8bc00ba72a493af76247c901659768cf
fcatus <f.catus@pm.me>
2020-07-03 14:39:06 -0500

    add cheat sheets

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     DeepLabCut: ['Getting Started', 'dlc-config', 'dlc-slurm', 'dlc-containers'],
-    HCC: ['singularity', 'RClone']
+    HCC: ['singularity', 'RClone'],
+    "Cheat Sheets": ['conda', 'sqlite', 'tmux']
   },
 };
