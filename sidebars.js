dac1283ade6e3d105d61a7847c1d422a4e67552e
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-07-08 18:42:40 -0500

    :wrench: Update homepage and sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
+  	Workflows: ['dlc-workflow'],
     DeepLabCut: ['Getting Started', 'dlc-config', 'dlc-slurm', 'dlc-containers'],
-    Workflows: ['dlc-workflow'],
     HCC: ['singularity', 'RClone'],
     "Cheat Sheets": ['conda', 'sqlite', 'pgsql', 'tmux', 'r'],
     Resources: ['felidae_ethogram']
