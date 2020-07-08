98ae32e1f191bade33d5a883ba21d3065513e164
fcatus <f.catus@pm.me>
2020-07-08 16:58:08 -0500

    :pencil: Add DLC Workflow page

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     DeepLabCut: ['Getting Started', 'dlc-config', 'dlc-slurm', 'dlc-containers'],
+    Workflows: ['dlc-workflow'],
     HCC: ['singularity', 'RClone'],
     "Cheat Sheets": ['conda', 'sqlite', 'pgsql', 'tmux', 'r'],
     Resources: ['felidae_ethogram']
