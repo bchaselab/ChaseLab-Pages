087aba8e20141430c5637731b3a19063a6eda88e
fcatus <f.catus@pm.me>
2020-07-19 13:48:00 -0500

    :art: Improve format of the code

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-  	Workflows: ["dlc-workflow", "dlc-workflow-cont"],
+    Workflows: ["dlc-workflow", "dlc-workflow-cont"],
     DeepLabCut: ["Getting Started", "dlc-config", "dlc-slurm", "dlc-containers"],
     HCC: ["singularity", "RClone"],
     "Cheat Sheets": ["conda", "sqlite", "pgsql", "tmux", "r"],
