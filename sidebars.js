baed33a79015ec6f5120557d45ca662495c2b79c
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-07-19 13:20:11 -0500

    :art: Improve code quality

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
   someSidebar: {
-  	Workflows: ['dlc-workflow', 'dlc-workflow-cont'],
-    DeepLabCut: ['Getting Started', 'dlc-config', 'dlc-slurm', 'dlc-containers'],
-    HCC: ['singularity', 'RClone'],
-    "Cheat Sheets": ['conda', 'sqlite', 'pgsql', 'tmux', 'r'],
-    Resources: ['felidae_ethogram']
+  	Workflows: ["dlc-workflow", "dlc-workflow-cont"],
+    DeepLabCut: ["Getting Started", "dlc-config", "dlc-slurm", "dlc-containers"],
+    HCC: ["singularity", "RClone"],
+    "Cheat Sheets": ["conda", "sqlite", "pgsql", "tmux", "r"],
+    Resources: ["felidae_ethogram"]
   },
 };
