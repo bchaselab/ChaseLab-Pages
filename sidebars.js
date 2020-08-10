09b0c9a225639b9175532958e0e8876d0abb1e1e
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-10 03:55:25 -0500

    Add Rome lint

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
-  someSidebar: {
-    Workflows: ["dlc-workflow", "dlc-workflow-cont"],
-    DeepLabCut: ["Getting_Started", "dlc-config", "dlc-slurm", "dlc-containers"],
-    HCC: ["singularity", "RClone"],
-    "Cheat Sheets": ["conda", "sqlite", "pgsql", "mongoDB", "tmux", "r"],
-    Resources: ["felidae_ethogram"]
-  },
+	someSidebar: {
+		Workflows: ["dlc-workflow", "dlc-workflow-cont"],
+		DeepLabCut: ["Getting_Started", "dlc-config", "dlc-slurm", "dlc-containers"],
+		HCC: ["singularity", "RClone"],
+		"Cheat Sheets": ["conda", "sqlite", "pgsql", "mongoDB", "tmux", "r"],
+		Resources: ["felidae_ethogram"],
+	},
 };
