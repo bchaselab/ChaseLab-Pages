acce94db3bde204af9d9d93da873cdc67a52648c
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-11 16:21:47 -0500

    :truck: Rename file

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
 	someSidebar: {
 		Workflows: ["dlc-workflow", "dlc-workflow-cont"],
-		DeepLabCut: ["Getting_Started", "dlc-config", "dlc-slurm", "dlc-containers"],
+		DeepLabCut: ["Getting_Started", "dlc-config", "dlc-crane", "dlc-containers"],
 		HCC: ["singularity", "RClone"],
 		"Cheat Sheets": ["conda", "sqlite", "pgsql", "mongoDB", "tmux", "r"],
 		Resources: ["felidae_ethogram"],
